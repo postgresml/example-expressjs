e12045ef70fb9cf6625c6a944bcbe6588f2eb3c9
Dan <39170265+chillenberger@users.noreply.github.com>
2024-03-18 15:13:30 -0600

    add express app

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# example-expressjs
-Example PostgresML application using Express JS
+# Express JS PostgresML example
+
+## Overview
+The purpose of this repository is to demonstrates how to quickly get up and running with machine learning in an Express application. 
+
+This application is a simple note taking application.  It analyzes all the notes taken in a day and performs summarization and sentiment analysis on them. 
+
+All our ML occurs in the database where the notes are stored.  This means just by connecting to the database we are able to train and deploy ML/AI models and have a production ready pipeline. 
+
+## Setup
+This example application requires a PostgreSQL database with the pgml and pgvector extension installed. The easiest way to do this is with a free database at [postgresml.org](https://postgresml.org/).
+
+Clone this repository.  Once you have your PostgresML database, or local PostgreSQL with the extensions installed, create a .env file and add `DATABASE_URL=<your_db_url>` replacing your_db_url. 
+
+Next install all required node packages with `node install`.
+
+## Running the app
+Start the express server by running the following 
+
+```bash
+npm run devStart
+```
+
+This will launch the application at [localhost:3000](http://localhost:3000/)
+
+## Usage
+In a browser, navigate to localhost:3000.  Recored a note in the text area on the left and click submit.  Do this a couple times.  On the right click Analyze Day,  this will  produce a sentiment analysis score and a summarization of your day.  
+
+## Code
+We can augment this code to perform all types of ML.  Checkout the [postgresML docs](https://postgresml.org/docs/api/sql-extension/) for a full list.
+
+If you are unhappy with the quality of the results, rest assured, higher quality models are available. 
+
+In this example we directly interacted with the extension using PostgreSQL.  If you would prefer JS, see our [JS SDK](https://postgresml.org/docs/api/client-sdk/)
---
+++ server.js
@@ -0,0 +1,124 @@
+import express from "express"
+import {Connection} from 'postgresql-client';
+import 'dotenv/config'
+
+const app = express()
+const port = 3000
+const connection = new Connection(process.env.DATABASE_URL)
+await connection.connect()
+
+app.set('view engine', 'ejs');
+app.use(
+  express.static('assets'), 
+  express.json(),
+  express.urlencoded({ extended: true })
+);
+
+app.get("/", async (req, res) => {
+  // Get all the notes to show 
+  let db_notes_req = `SELECT * FROM notes WHERE DATE(created_at) = DATE(NOW());`
+  const db_note_resp = await connection.execute(db_notes_req)
+  const notes = db_note_resp.results[0].rows
+
+  // Get the summary and score of the day
+  let db_day_req = `SELECT * FROM days WHERE DATE(created_at) = DATE(NOW());`
+  const db_day_resp = await connection.execute(db_day_req)
+
+  let description = db_day_resp.results[0].rows[0]?.[1]
+  let score = db_day_resp.results[0].rows[0]?.[2]
+
+  res.render('index', { data: notes.reverse(), score: score, description: description})
+})
+
+
+
+app.post("/add", async (req, res) => {
+  ///  escape single quotes
+  let text = req.body.note.replaceAll("'", "''")
+
+  /// Construct a SQL request that that performs sentiment analysis when entered into database:
+  /// 
+  /// 1. Analyzes the sentiment of the note
+  /// 2. Converts sentiment score to +/1 or 0 dependingn on pos, neg, or neutral
+  /// 3. Inserts the note and the sentiment score into the notes table
+  let db_request = `
+    WITH note AS (
+      SELECT pgml.transform(
+        inputs => ARRAY['${text}'],
+        task => '{"task": "text-classification", "model": "finiteautomata/bertweet-base-sentiment-analysis"}'::JSONB
+      ) AS market_sentiment
+    ), 
+
+    score AS (
+      SELECT 
+        CASE 
+          WHEN (SELECT market_sentiment FROM note)[0]::JSONB ->> 'label' = 'POS' THEN 1
+          WHEN (SELECT market_sentiment FROM note)[0]::JSONB ->> 'label' = 'NEG' THEN -1
+          ELSE 0
+        END AS score
+    )
+
+    INSERT INTO notes (note, score) VALUES ('${text}', (SELECT score FROM score));
+  `
+
+  await connection.execute(db_request);
+
+  res.redirect('/')
+})
+
+app.get("/anaylze", async (req, res) => {
+
+  /// Construct a SQL request compute daily analysis and summarization of notes:
+  /// 1. Get all notes from today
+  /// 2. Summarize the notes
+  /// 3. Insert the summary and the sentiment score into the days table
+  let db_request = `
+  WITH day AS (
+    SELECT 
+      note,
+      score
+    FROM notes 
+    WHERE DATE(created_at) = DATE(NOW())),
+
+    sum AS (
+      SELECT pgml.transform(
+        task => '{"task": "summarization", "model": "sshleifer/distilbart-cnn-12-6"}'::JSONB,
+        inputs => array[(SELECT STRING_AGG(note, '\n') FROM day)],
+        args => '{"min_length" : 20, "max_length" : 70}'::JSONB
+      ) AS summary
+    )
+
+    INSERT INTO days (summary, score) 
+    VALUES ((SELECT summary FROM sum)[0]::JSONB ->> 'summary_text', (SELECT SUM(score) FROM day))
+    On Conflict (created_at) DO UPDATE SET summary=EXCLUDED.summary, score=EXCLUDED.score 
+    RETURNING score;
+  `
+
+  await connection.execute(db_request);
+
+  res.redirect('/')
+})
+
+app.listen(port, async () => {
+  // Create the notes table if it does not exist
+  const notes = await connection.execute(`
+    CREATE TABLE IF NOT EXISTS notes ( 
+      id BIGSERIAL PRIMARY KEY, 
+      note VARCHAR(5000), 
+      score FLOAT, 
+      created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    );`
+  )
+
+  // Create the days table if it does not exist
+  const day = await connection.execute(`
+    CREATE TABLE IF NOT EXISTS days ( 
+      id BIGSERIAL PRIMARY KEY, 
+      summary VARCHAR, 
+      score FLOAT, 
+      created_at DATE NOT NULL UNIQUE DEFAULT DATE(NOW())
+    );`
+  )
+
+  console.log(`Server is running at http://localhost:${port}`)
+})
